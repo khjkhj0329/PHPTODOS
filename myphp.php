f989fe0d163598d28d7c4632d32266953a08c29c
khjkhj0329 <khjkhj0329@gmail.com>
2021-07-01 07:15:06 +0000

    PHP 실습

---
+++ myphp.php
@@ -0,0 +1,22 @@
+<?php
+include('showerror.php');
+?>
+
+HTML 영역
+print("Hello");
+<?php
+print("Hello\n");
+if(false){
+	print("Hello\n");
+	print("Hello");
+	print("Hello\n");
+}
+
+$x = 3;
+$x = "무아호";
+$value = 1000000000000;
+print $value."<br>";
+$value = $value + 1;
+print $value."<br>";
+
+?>
